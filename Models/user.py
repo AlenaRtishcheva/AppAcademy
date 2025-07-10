5b28ded3f0e1091f99d7c22bde1500f0f50c7259
lori <lorinozarova@yandex.ru>
2025-07-10 20:54:09 +0300

    add_model_user
    добавлена модель юзер

---
+++ Models/user.py
@@ -0,0 +1,15 @@
+
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    def get_group(self):
+        #вставить миграчию функцию
+        pass
+    def get_tutor(self):
+        #вставить миграчию функцию
+        pass
+    def get_departments(self):
+        #вставить миграчию функцию
+        pass
